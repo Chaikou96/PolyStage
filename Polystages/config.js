eb3fb18b39c95f5c3f78931058ad732320c2bc7b
Aymen Damak <aymen.damak@yahoo.fr>
2020-10-16 19:49:46 +0200

    .

---
+++ Polystages/config.js
@@ -0,0 +1,20 @@
+const config = {
+  app: {
+    port: 8080
+  },
+  db: {
+    host: 'localhost',
+    port: 3306,
+    database: 'phpmyadmin',
+    user: 'phpmyadmin',
+    password: 'root'
+  },
+  gmail: {
+    user: 'bobocheck2006@gmail.com',
+    mdp: '...'
+  }
+};
+
+
+module.exports = config;
+
